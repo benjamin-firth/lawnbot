4851edaf15fa0b0e6ab9e0ab8f2b6d00802d4d23
Ben Firth <bfirth93@gmail.com>
2020-11-09 13:33:52 -0700

    Create welcome form basic structure

---
+++ src/components/WelcomeForm/WelcomeForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WelcomeForm = () => {
+  return (
+    <section>
+      <h3>Thanks for filling in your lawn area!</h3>
+      <form>
+        <label>Your approximate lawn size</label>
+        <input />
+      </form>
+    </section>
+  );
+};
+
+export default WelcomeForm;
