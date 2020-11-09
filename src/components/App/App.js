1e08b4b8f137dcc03768214959698438248248d3
Ben Firth <bfirth93@gmail.com>
2020-11-09 13:39:52 -0700

    Add styled-components dependancy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from '../Navbar/Navbar';
 import WelcomeForm from '../WelcomeForm/WelcomeForm';
 import LawnPreviewContainer from '../LawnPreviewContainer/LawnPreviewContainer';
