a78423bd289e5c6a03828bd7b71504083d0ae45f
Ben Firth <bfirth93@gmail.com>
2020-11-09 13:33:18 -0700

    Create lawn preview card basic structure

---
+++ src/components/LawnPreviewCard/LawnPreviewCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LawnPreviewCard = () => {
+  return (
+    <article>
+      <div>Image here</div>
+      <p>Services Provided:</p>
+      <ul>
+        <li>example</li>
+        <li>example</li>
+        <li>example</li>
+      </ul>
+    </article>
+  );
+};
+
+export default LawnPreviewCard;
