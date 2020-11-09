6240e9ee26a48a033d504986c407ef74a3e7b9f1
Ben Firth <bfirth93@gmail.com>
2020-11-09 13:33:30 -0700

    Create lawn preview container basic structure

---
+++ src/components/LawnPreviewContainer/LawnPreviewContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LawnPreviewCard from '../LawnPreviewCard/LawnPreviewCard';
+
+const LawnPreviewContainer = () => {
+  return (
+    <section>
+      <LawnPreviewCard />
+      <LawnPreviewCard />
+      <LawnPreviewCard />
+    </section>
+  );
+};
+
+export default LawnPreviewContainer;
