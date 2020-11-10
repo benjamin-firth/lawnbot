2b0f17513af214333c792d990d9353af0a71a670
Ben Firth <bfirth93@gmail.com>
2020-11-10 09:36:45 -0700

    Add basic styling to lawn preview card

--- src/components/LawnPreviewCard/LawnPreviewCard.js
+++ src/components/LawnPreviewCard/LawnPreviewCard.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const LawnPreviewCard = () => {
   return (
-    <article>
-      <div>Image here</div>
+    <StyledPreviewCard>
+      <img src='https://www.mypurelawn.com/wp-content/uploads/before-after3.jpg'/>
       <p>Services Provided:</p>
       <ul>
         <li>example</li>
         <li>example</li>
         <li>example</li>
       </ul>
-    </article>
+    </StyledPreviewCard>
   );
 };
+
+const StyledPreviewCard = styled.article`
+  background-color: #fff;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  padding: 10px;
+  width: 550px;
+`;
 
 export default LawnPreviewCard;
